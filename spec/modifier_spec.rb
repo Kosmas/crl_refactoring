1bcf9e0b393be0f14d8af8c8b68c7fd3729cf6c2
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-06-30 16:17:50 +0200

    add first spec for non existing files

--- spec/modifier_spec.rb
+++ spec/modifier_spec.rb
@@ -2,4 +2,16 @@ require File.expand_path('spec_helper', File.dirname(__FILE__))
 require 'modifier'
 
 describe Modifier do
+  let(:saleamount_factor) { 1 }
+  let(:cancellation_factor) { 0.4 }
+  let(:input) { 'input.txt' }
+  let(:output) { 'output.txt' }
+
+  before :each do
+    @modifier = Modifier.new(saleamount_factor, cancellation_factor)
+  end
+
+  it 'displays error when file not found' do
+    expect(@modifier.modify(input, output)).to raise_error('No such file or directory')
+  end
 end
