090eb426358f6e87c197f6696172da2481690b5c
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-06-30 14:32:43 +0200

    remove redundant self keywords

--- modifier.rb
+++ modifier.rb
@@ -20,13 +20,13 @@ end
 
 class String
   def from_german_to_f
-    self.gsub(',', '.').to_f
+    gsub(',', '.').to_f
   end
 end
 
 class Float
   def to_german_s
-    self.to_s.gsub('.', ',')
+    to_s.gsub('.', ',')
   end
 end
 
